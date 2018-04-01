bf65f91076daa597dae00a1633474ff131a4bb7b
richplastow <rich@richplastow.com>
2018-04-01 20:48:31 +0530

    0.0.3 Basic working SSE server with example client

--- oompsh.js
+++ oompsh.js
@@ -1,8 +1,67 @@
-const server = (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('This is Oompsh ' + require('./package.json').version + '\n')
-}
 const app = require('http').createServer(server)
 const port = process.env.PORT || 3000 // Heroku sets $PORT
 app.listen( port, () => console.log(`App is listening on port ${port}`) )
+
+
+//// from www.html5rocks.com/en/tutorials/eventsource/basics/#toc-server-code
+function server (req, res) {
+
+    //// Deal with a version request.
+    if ('/version' === req.url && 'GET' === req.method) {
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end('Oompsh ' + require('./package.json').version + '\n')
+    }
+
+    //// Deal with a request to listen for SSEs.
+    else if ('/events' === req.url && 'GET' === req.method) {
+        if (req.headers.accept && 'text/event-stream' === req.headers.accept) {
+            sendSSE(req, res)
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'})
+            res.end('Error: ‘accept’ header is not ‘text/event-stream’\n')
+        }
+    }
+
+    //// Deal with a request for the example client page.
+    else if ( ('/' === req.url || '/index.html' === req.url) && 'GET' === req.method) {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end( require('fs').readFileSync(__dirname + '/index.html') )
+    }
+
+    //// Deal with a notification on the private channel.
+    else if ('/notify' === req.url && 'POST' === req.method) {
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end('@TODO deal with notify\n')
+    }
+
+    //// Not found.
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end('Not found\n')
+    }
+
+}
+
+function sendSSE (req, res) {
+    res.writeHead(200, {
+        'Content-Type':  'text/event-stream'
+      , 'Cache-Control': 'no-cache'
+      , 'Connection':    'keep-alive'
+    })
+
+    const id = (new Date()).toLocaleTimeString()
+
+    //// Send an SSE every two seconds on a single connection.
+    setInterval(function() {
+        constructSSE( res, id, { time:(new Date()).toLocaleTimeString() } )
+    }, 2000)
+
+    constructSSE( res, id, { time:(new Date()).toLocaleTimeString() } )
+}
+
+function constructSSE(res, id, data) {
+    res.write('id: ' + id + '\n')
+    JSON.stringify(data, 2).split('\n').forEach(
+        line => res.write('data: ' + line + '\n') )
+    res.write('\n')
+}
