ef22957bfdfb34e5bdad8b937f5f93e470ee6508
richplastow <rich@richplastow.com>
2018-04-07 12:22:51 +0530

    qik Heroku CORS fix5

--- oompsh.js
+++ oompsh.js
@@ -22,7 +22,7 @@ function server (req, res) {
     //// Set default headers. Note that 'Content-Type' will be overridden for
     //// an SSE connection.
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Credentials', true)
+    // res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length')
